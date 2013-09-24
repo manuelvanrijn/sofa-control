5bde177c05b7f3127b20cfbf36460fdeaa70bd45
Manuel van Rijn <vanrijn@auxilium.nl>
2013-09-24 17:38:55 +0200

    added nzbindex as default search provider

--- assets/javascripts/controllers/sabnzbd_controller.js
+++ assets/javascripts/controllers/sabnzbd_controller.js
@@ -1,10 +1,11 @@
 (function() {
   'use strict';
 
-  sofaControlApp.controller('SabnzbCtrl', ['$scope', '$rootScope', 'SABnzbdService',
-    function($scope, $rootScope, SABnzbdService) {
+  sofaControlApp.controller('SabnzbCtrl', ['$scope', '$rootScope', 'SABnzbdService', 'NZBIndexService',
+    function($scope, $rootScope, SABnzbdService, NZBIndexService) {
       $scope.queue = [];
       $scope.history = [];
+      $scope.searchResult = {};
 
       $scope.getHistory = function() {
         SABnzbdService.history().then(function(data) {
@@ -20,6 +21,29 @@
         var minutes = ('0' + parseInt((totalSeconds / 60) % 60)).slice(-2);
         var hours = ('0' + parseInt(totalSeconds / 3600)).slice(-2);
         return hours + ':' + minutes + '.' + seconds;
+      };
+
+      $scope.search = function(query) {
+        NZBIndexService.search(query).then(function(response) {
+          $scope.searchResult = response.data.responseData.feed;
+        }, function(error) {
+          $scope.searchResult = {};
+          console.log(error);
+        });
+      };
+
+      $scope.download = function(entry) {
+        window.$chocolatechip.UIPopup({
+          id: 'warning',
+          title: 'Confirm download',
+          message: 'Do you want to download: <br />' + entry.title + '?',
+          cancelButton: 'Cancel',
+          continueButton: 'Download',
+          callback: function() {
+            var link = entry.link.replace('/release/', '/download/');
+            SABnzbdService.addTaskByUrl(link, entry.title);
+          }
+        });
       };
 
       var updateQueue = function() {
---
+++ assets/javascripts/services/nzb-providers/nzbindex.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  sofaControlApp.service('NZBIndexService', ['$http', '$q', function($http, $q) {
+    this.search = function(query) {
+      var defer = $q.defer();
+      var url = 'http://www.nzbindex.nl/rss/?q=' + query + '&sort=agedesc&hidecross=1&max=100&more=1';
+      var _this = this;
+      _this.shortUrl(url).then(function(resp) {
+        var url = resp.data.id;
+        _this.processUrl(defer, url, 1);
+      });
+      return defer.promise;
+    };
+
+    // Because nzbindex isn't very stable, it might fail a few times.
+    // Let's try it 10 times and after the 10th try return an error
+    this.processUrl = function(defer, url, times) {
+      var randUrl = url + '?rand=' + Math.random();
+      var _this = this;
+      _this.feedToJson(randUrl).then(function(response) {
+        if(times === 10) {
+          defer.reject('failed after 10 times');
+        }
+        else {
+          if (response.data.responseStatus !== 200) {
+            _this.processUrl(defer, url, (times+1));
+          }
+          else {
+            defer.resolve(response);
+          }
+        }
+      });
+    };
+
+    this.feedToJson = function(feedUrl) {
+      var url = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&callback=JSON_CALLBACK&q=' + feedUrl;
+      return $http.jsonp(url);
+    };
+
+    this.shortUrl = function(url) {
+      return $http.post('https://www.googleapis.com/urlshortener/v1/url', {
+        longUrl: url
+      });
+    };
+
+  }]);
+}).call(this);
